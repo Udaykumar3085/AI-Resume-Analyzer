e61a7d3689cb469d76fdf1847cd867e361d67bdf
Udaykumar Basappa Mechchanavar <udaykumarbmechanavar2004@gmail.com>
2026-07-17 17:46:55 +0530

    Day 4 completed - Resume Upload API

---
+++ backend/app/api/resume_api.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, UploadFile, File, HTTPException
+import os
+import shutil
+
+router = APIRouter()
+
+UPLOAD_FOLDER = "../uploads"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+@router.post("/upload-resume")
+async def upload_resume(file: UploadFile = File(...)):
+    allowed_extensions = [".pdf", ".docx"]
+
+    extension = os.path.splitext(file.filename)[1].lower()
+
+    if extension not in allowed_extensions:
+        raise HTTPException(
+            status_code=400,
+            detail="Only PDF and DOCX files are allowed."
+        )
+
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "message": "Resume uploaded successfully",
+        "filename": file.filename
+    }
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.database.database import Base, engine
 from app.models.user import User
 from app.api.user_api import router as user_router
+from app.api.resume_api import router as resume_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -11,6 +12,7 @@ app = FastAPI(
 )
 
 app.include_router(user_router)
+app.include_router(resume_router)
 
 
 @app.get("/")
